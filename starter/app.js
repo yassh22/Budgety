0a005a1f2101771fa9e749ecb909ac5323f2f4bc
Yash Panchal <yashvardan@Dark-Empire.local>
2019-12-11 19:25:46 -0500

    feat: designed the main architecture of the app and created modules.

---
+++ starter/app.js
@@ -0,0 +1,36 @@
+// Budget Controller
+let budgetController = (function(){
+
+
+
+
+})();
+
+// UI Controller
+let UIController = (function(){
+
+
+
+
+})();
+
+// Global App Controller
+let controller = (function(budgetCtrl, UICtrl){
+
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    
+    // 1. Get the field input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
+  });
+
+
+
+})(budgetController, UIController);
